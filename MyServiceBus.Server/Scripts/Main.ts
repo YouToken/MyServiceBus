ce7fc0961c4cfe9f244688ddddae485c6be64a5e
amigin <amigin@gmail.com>
2020-08-30 16:02:47 +0300

    Improved requesting algo

--- MyServiceBus.Server/Scripts/Main.ts
+++ MyServiceBus.Server/Scripts/Main.ts
@@ -6,19 +6,31 @@ class Main{
     
     private static bodyElement: Element;
     
+    private static requesting = false;
+    
     public static request():void {
 
         if (!this.bodyElement)
             this.bodyElement = document.getElementsByTagName("BODY")[0];
+        
+        if (this.requesting)
+            return;
+
+        this.requesting = true;
 
         $.ajax({url: '/Monitoring'})
             .then((r: IMonitoringInfo) => {
+                
+                this.requesting = false;
 
                 this.bodyElement.innerHTML =
                     HtmlTopicRenderer.renderTable(r.topics, r.connections)
                     + HtmlConnectionsRenderer.renderConnectionsTable(r.connections)
                     + HtmlQueueToPersistRenderer.RenderQueueToPersistTable(r.queueToPersist);
 
+            })
+            .fail(()=>{
+                this.requesting = false; 
             });
     }
     
