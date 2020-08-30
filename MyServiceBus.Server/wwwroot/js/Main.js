a7b94a88d043114a9ca6644a5427623d4047c4d1
amigin <amigin@gmail.com>
2020-08-30 16:07:25 +0300

    Improved requesting algo

--- MyServiceBus.Server/wwwroot/js/Main.js
+++ MyServiceBus.Server/wwwroot/js/Main.js
@@ -5,14 +5,22 @@ var Main = /** @class */ (function () {
         var _this = this;
         if (!this.bodyElement)
             this.bodyElement = document.getElementsByTagName("BODY")[0];
+        if (this.requesting)
+            return;
+        this.requesting = true;
         $.ajax({ url: '/Monitoring' })
             .then(function (r) {
+            _this.requesting = false;
             _this.bodyElement.innerHTML =
                 HtmlTopicRenderer.renderTable(r.topics, r.connections)
                     + HtmlConnectionsRenderer.renderConnectionsTable(r.connections)
                     + HtmlQueueToPersistRenderer.RenderQueueToPersistTable(r.queueToPersist);
+        })
+            .fail(function () {
+            _this.requesting = false;
         });
     };
+    Main.requesting = false;
     return Main;
 }());
 window.setInterval(function () {
