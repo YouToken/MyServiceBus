620db3288e49467f18efed415a29a31850b720aa
Andrey <amigin@gmail.com>
2021-03-12 22:12:10 +0200

    Fixed Renering the page

--- MyServiceBus.Server/Scripts/HtmlTopicRenderer.ts
+++ MyServiceBus.Server/Scripts/HtmlTopicRenderer.ts
@@ -6,7 +6,7 @@ class HtmlTopicRenderer {
     for (let page of pages) {
       result +=
           '<div class="progress">' +
-          '<div class="progress-bar" role="progressbar" style="width: ' + page.percent + '%;" aria-valuenow="25" aria-valuemin="0" aria-valuemax="100">' +
+          '<div class="progress-bar" role="progressbar" style="color:black; width: ' + page.percent + '%;" aria-valuenow="25" aria-valuemin="0" aria-valuemax="100">' +
           page.label +
           '</div>' +
           "<div>";
--- MyServiceBus.Server/wwwroot/js/HtmlTopicRenderer.js
+++ MyServiceBus.Server/wwwroot/js/HtmlTopicRenderer.js
@@ -7,7 +7,7 @@ var HtmlTopicRenderer = /** @class */ (function () {
             var page = pages_1[_i];
             result +=
                 '<div class="progress">' +
-                    '<div class="progress-bar" role="progressbar" style="width: ' + page.percent + '%;" aria-valuenow="25" aria-valuemin="0" aria-valuemax="100">' +
+                    '<div class="progress-bar" role="progressbar" style="color:black; width: ' + page.percent + '%;" aria-valuenow="25" aria-valuemin="0" aria-valuemax="100">' +
                     page.label +
                     '</div>' +
                     "<div>";
